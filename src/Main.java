5b525b11e137c367d3fdb53d9abbf617030ded0e
Trevor-Hartman <viable2005@gmail.com>
2025-05-14 13:15:25 -0700

    Working API to Character Example

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.Request;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+
 import java.io.InputStream;
 
 public class Main {
+    public static Character loadCharacterFromApi(String url) throws Exception {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        Response response = client.newCall(request).execute();
+        String jsonStr = response.body().string();
+        //System.out.println(jsonStr);
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(jsonStr, Character.class);
+    }
+
     public static Character loadCharacterFromResources(String resourcePath) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
 
@@ -17,7 +34,8 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Character character = loadCharacterFromResources("character.json");
+            //Character character = loadCharacterFromResources("character.json");
+            Character character = loadCharacterFromApi("http://127.0.0.1:5000/player/100");
             System.out.println("Loaded character: " + character);
         } catch (Exception e) {
             e.printStackTrace();
